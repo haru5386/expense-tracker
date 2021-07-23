6b80c0b90616688b2abc84dc9de90cda075a10ee
haru5386 <snsn550@gmail.com>
2021-07-23 11:07:55 +0800

    feat: delete function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const app = express()
 
 const Record = require('./models/Record')
@@ -14,6 +15,7 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   Promise.all([Record.find().lean(), Category.find().lean()])
@@ -42,6 +44,18 @@ app.get('/records/new', (req, res) => {
     .then((category) => {
       res.render('new', { category })
     })
+    .catch(error => {
+      console.log(error)
+      res.render('index', { errMsg: error.message })
+    })
+})
+
+app.delete('/records/:id', (req, res) => {
+  const id = req.params.id
+  console.log(id)
+  Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
     .catch(error => {
       console.log(error)
       res.render('index', { errMsg: error.message })
