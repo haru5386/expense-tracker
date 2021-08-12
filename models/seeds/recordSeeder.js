95f59ff3e280ed925815283074214857c5428a14
haru5386 <snsn550@gmail.com>
2021-08-13 07:39:01 +0800

    h

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -14,21 +14,21 @@ const SEED_USER = [{
   password: '12345678',
 }]
 
-// db.once('open', () => {
-//   Promise.all(Array.from(SEED_USER, (SEED_USER, i) => {
-//     return bcrypt
-//       .genSalt(10)
-//       .then(salt => bcrypt.hash(SEED_USER.password, salt))
-//       .then(hash => User.create({ name: SEED_USER.name, email: SEED_USER.email, password: hash }))
-//       .then(user => {
-//         const userId = user._id
-//         recordSeeds.forEach(record => record.userId = userId)
-//         return Record.create(recordSeeds)
-//       })
-//   }))
-//     .then(() => {
-//       console.log('User Record Seed done')
-//       process.exit()
-//     })
-//     .catch(error => console.log(error))
-// })
+db.once('open', () => {
+  Promise.all(Array.from(SEED_USER, (SEED_USER, i) => {
+    return bcrypt
+      .genSalt(10)
+      .then(salt => bcrypt.hash(SEED_USER.password, salt))
+      .then(hash => User.create({ name: SEED_USER.name, email: SEED_USER.email, password: hash }))
+      .then(user => {
+        const userId = user._id
+        recordSeeds.forEach(record => record.userId = userId)
+        return Record.create(recordSeeds)
+      })
+  }))
+    .then(() => {
+      console.log('User Record Seed done')
+      process.exit()
+    })
+    .catch(error => console.log(error))
+})
